0ef768342c218cbcc7f0ae0722b2cd7d65a0f590
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-05-25 07:57:48 +0530

    Gibberish App

---
+++ app/src/main/java/com/ajaybhatia/gibberish/MainActivity.java
@@ -0,0 +1,46 @@
+package com.ajaybhatia.gibberish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.nameEditText);
+        textView = (TextView) findViewById(R.id.resultTextView);
+    }
+
+
+    public void gibberish(View view) {
+        String name = editText.getText().toString();
+
+        List<Character> list = new ArrayList<>();
+        for (int i = 0; i < name.length(); i++)
+            list.add(name.charAt(i));
+
+        Collections.shuffle(list);
+
+        StringBuilder s = new StringBuilder();
+        for (Character c : list)
+            s.append(c);
+
+        textView.setText(s.toString());
+
+    }
+}
